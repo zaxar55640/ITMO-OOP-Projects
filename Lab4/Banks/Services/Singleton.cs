03bc9229250b0d055a74383db378eee24ff98e71
zaxar55640 <zaxar55640@gmail.com>
2022-12-08 18:05:46 +0300

    123
    брат убери нож

---
+++ Lab4/Banks/Services/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace Banks.Services;
+
+public class Singleton
+{
+    private static Singleton? instance;
+
+    protected Singleton()
+    {
+    }
+
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+}
